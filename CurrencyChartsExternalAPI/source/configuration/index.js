14682be97f1af447f306a899c006a8a46661b8ea
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2017-04-23 00:32:55 -0300

    Add Dockefile

--- CurrencyChartsExternalAPI/source/configuration/index.js
+++ CurrencyChartsExternalAPI/source/configuration/index.js
@@ -1,10 +1,19 @@
+import process from 'process';
 import dotenv from 'dotenv';
 
 const configuration = dotenv.config().parsed;
 
 export const CURRENCY_LAYER_API_URL =
-    configuration.CURRENCY_LAYER_API_URL || '';
+    process.env.CURRENCY_LAYER_API_URL ||
+    configuration.CURRENCY_LAYER_API_URL ||
+    'http://apilayer.net/api';
+
 export const CURRENCY_LAYER_API_TOKEN =
-    configuration.CURRENCY_LAYER_API_TOKEN || '';
+    process.env.CURRENCY_LAYER_API_TOKEN ||
+    configuration.CURRENCY_LAYER_API_TOKEN ||
+    '';
+
 export const APPLICATION_PORT =
-    configuration.APPLICATION_PORT || '8088';
+    process.env.EXTERNAL_API_PORT ||
+    configuration.EXTERNAL_API_PORT ||
+    '8088';
